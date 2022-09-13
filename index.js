60b2c5f44730595cf7a023af78a15246d62a98c6
levideutsch <levideutsch@live.com>
2022-09-13 15:23:35 -0400

    commit tree 8805dd597175ba9cd7e6569c431cebf4e9a11835

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(stringNames, eventName) {
+    let cardsRay = []
+     for (let a = 0; a < stringNames.length; a++) 
+     cardsRay.push(`Thank you, ${stringNames[a]}, for the wonderful ${eventName} gift!`)
+     return cardsRay
+ } 
+
+ function countDown(number) {
+    while (number > 0)
+    {console.log(number)
+    number--}
+    console.log(number)
+}
+countDown(11)
